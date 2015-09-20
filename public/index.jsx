4bc4b11577e4bfc664afdf5b64273839143fd340
Henrique Ramos Limas <henrique.ramos.limas@gmail.com>
2015-09-20 18:38:46 -0300

    feat(Picture): create picture layout.

--- public/index.jsx
+++ public/index.jsx
@@ -4,4 +4,4 @@ var React = require('React');
 
 var Navbar = require('./components/navbar/navbar.jsx');
 
-React.render(<Navbar appName="Insta React"/>, document.getElementById('content'));
+React.render(<Navbar appName="Insta React"/>, document.getElementById('navbar'));
